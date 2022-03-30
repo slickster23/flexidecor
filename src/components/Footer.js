ba4ff05a0b4f05f224ae0d68248572f36d27100e
slickster23 <tokelo.foso23@gmail.com>
2022-03-30 23:40:15 +0200

    Added Products

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 function Footer() {
   return (
     <div id = "footer">
-    <p>Copyright &copy; Flexi Decor 2022. Designed & Developed by <a href="https://www.tokelofoso.online" target="_blank">Tokelo Foso</a></p>
+    <p>Copyright &copy; Flexi Decor 2022. </p>
 
     </div>
   )
